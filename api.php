a63f84d2fdcaa9bb658bbe032bc25018e1aa9a13
Maurits van der Schee <maurits@vdschee.nl>
2015-02-12 02:27:32 +0100

    Added stand-alone detection

--- api.php
+++ api.php
@@ -296,5 +296,9 @@ class MySQL_CRUD_API {
 	}
 }
 
-//$api = new MySQL_CRUD_API("localhost","user","pass","db",false,array("users"=>"crudl"));
-//$api->executeCommand();
+// only execute this when running in stand-alone mode
+if(count(get_required_files())<2) {
+	// parameters: hostname, username, password, database, whitelist, blacklist
+	$api = new MySQL_CRUD_API("localhost","user","pass","db",false,array("users"=>"crudl"));
+	$api->executeCommand();
+}
