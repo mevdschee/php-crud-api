23c7d7fb7fadbf37ff5d5fa140fe7abfcec7a7c0
Maurits van der Schee <maurits@vdschee.nl>
2015-06-13 12:06:24 +0200

    Removed shell usage for loading fixture

--- tests.php
+++ tests.php
@@ -3,10 +3,10 @@ require "api.php";
 
 class MySQL_CRUD_API_Config
 {
-	public static $hostname='localhost';
-	public static $username='root';
-	public static $password='root';
-	public static $database='mysql_crud_api'; // NB: Use an empty database, data will be LOST!
+	public static $hostname='{{test_hostname}}';
+	public static $username='{{test_username}}';
+	public static $password='{{test_password}}';
+	public static $database='{{test_database}}'; // NB: Use an empty database, data will be LOST!
 }
 
 class API
