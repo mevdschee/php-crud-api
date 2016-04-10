860fa47a6452a2edc8251840a24b05e094d326b6
Maurits van der Schee <maurits@vdschee.nl>
2016-04-11 01:56:02 +0200

    Component based mustache example

--- examples/component.js
+++ examples/component.js
@@ -10,10 +10,10 @@ $(function(){
 		if (components[name]) return;
 		components[name] = true;
 
-		$.ajax({dataType:'text', url: name+'.css',success:function(data){
+		$.ajax({dataType:'text', url: './'+name+'.css',success:function(data){
 			$('<style>').appendTo('body').text(data);
 		}});
-		$.ajax({dataType:'text', url: name+'.html',success:function(data){
+		$.ajax({dataType:'text', url: './'+name+'.html',success:function(data){
 			template = data;
 			$('<script>').attr('src',name+'.js').appendTo('body').on('load',function(){
         $(['div.component[component="'+name+'"]']).each(function(){
