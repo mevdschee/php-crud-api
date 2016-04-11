704eab9104224686af62a13d823bd166c942a18a
Maurits van der Schee <maurits@vdschee.nl>
2016-04-12 00:09:46 +0200

    Updated w3component example

--- examples/w3component/w3component.js
+++ examples/w3component/w3component.js
@@ -2,16 +2,13 @@ var w3component = $(function(){
 	var self = this;
 	self.components = {};
 	var handleComponent = function(){
-		var w3c = $(this).attr('w3component').split('/');
-		var name = w3c.pop();
-		var path = '.';
-		if (w3c.length>0) path=w3c.join('/');
+		var name = $(this).attr('w3component');
 		if (self.components[name]) return;
 		self.components[name] = true;
-		$.ajax({dataType:'text', url: path+'/'+name+'.css',success:function(styles){
+		$.ajax({dataType:'text', url: name+'.css',success:function(styles){
 			$('<style>').appendTo('body').text(styles);
 		}});
-		$.ajax({dataType:'text', url: path+'/'+name+'.html',success:function(template){
+		$.ajax({dataType:'text', url: name+'.html',success:function(template){
 			$('<script>').attr('src',name+'.js').appendTo('body').on('load',function(){
 				$(['div.w3component[w3component="'+name+'"]']).each(function(){
 					self.components[name]($(this),template);
