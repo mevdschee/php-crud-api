2b30959bc2d2e887810bc565c08cabc676a029c2
Maurits van der Schee <maurits@vdschee.nl>
2016-05-19 00:43:04 +0200

    Avoid warnings on empty transformed resultset

--- lib/php_crud_api_transform.php
+++ lib/php_crud_api_transform.php
@@ -2,23 +2,25 @@
 function php_crud_api_transform(&$tables) {
 	$get_objects = function (&$tables,$table_name,$where_index=false,$match_value=false) use (&$get_objects) {
 		$objects = array();
-		foreach ($tables[$table_name]['records'] as $record) {
-			if ($where_index===false || $record[$where_index]==$match_value) {
-				$object = array();
-				foreach ($tables[$table_name]['columns'] as $index=>$column) {
-					$object[$column] = $record[$index];
-					foreach ($tables as $relation=>$reltable) {
-						if (isset($reltable['relations'])) {
-							foreach ($reltable['relations'] as $key=>$target) {
-								if ($target == "$table_name.$column") {
-									$column_indices = array_flip($reltable['columns']);
-									$object[$relation] = $get_objects($tables,$relation,$column_indices[$key],$record[$index]);
+		if (isset($tables[$table_name]['records'])) {
+			foreach ($tables[$table_name]['records'] as $record) {
+				if ($where_index===false || $record[$where_index]==$match_value) {
+					$object = array();
+					foreach ($tables[$table_name]['columns'] as $index=>$column) {
+						$object[$column] = $record[$index];
+						foreach ($tables as $relation=>$reltable) {
+							if (isset($reltable['relations'])) {
+								foreach ($reltable['relations'] as $key=>$target) {
+									if ($target == "$table_name.$column") {
+										$column_indices = array_flip($reltable['columns']);
+										$object[$relation] = $get_objects($tables,$relation,$column_indices[$key],$record[$index]);
+									}
 								}
 							}
 						}
 					}
+					$objects[] = $object;
 				}
-				$objects[] = $object;
 			}
 		}
 		return $objects;
