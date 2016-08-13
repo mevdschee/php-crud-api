12a5e1889da9652839a6b48cc467b4082ea27792
Maurits van der Schee <maurits@vdschee.nl>
2016-08-13 03:38:38 +0200

    improved readability

--- login_token.php
+++ login_token.php
@@ -5,7 +5,7 @@ require 'api.php';
 
 $auth = new PHP_API_AUTH(array(
 	'secret'=>'someVeryLongPassPhrase',
-	'authenticator'=>function($user,$pass){ $_SESSION['user']=($user=='admin' && $pass=='admin'); }
+	'authenticator'=>function($user,$pass){ if ($user=='admin' && $pass=='admin') $_SESSION['user']=$user; }
 ));
 $auth->executeCommand();
 ?>/>
