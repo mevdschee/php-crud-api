451e1ad50951440489912a0daeb9c1d121151b08
Maurits van der Schee <maurits@vdschee.nl>
2016-09-19 17:18:32 +0200

    Update auth.php

--- examples/auth.php
+++ examples/auth.php
@@ -4,7 +4,7 @@ if (!isset($_SERVER['PHP_AUTH_USER'])) {
     header('HTTP/1.0 401 Unauthorized');
     echo 'Text to send if user hits Cancel button';
     exit;
-} elseif ($_SERVER['PHP_AUTH_USER']!='user' && $_SERVER['PHP_AUTH_PW']!='pass') {
+} elseif ($_SERVER['PHP_AUTH_USER']!='user' || $_SERVER['PHP_AUTH_PW']!='pass') {
     header('HTTP/1.0 403 Forbidden');
     echo 'Forbidden';
     exit;
