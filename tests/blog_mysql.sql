09acba23c9515c4b56001652a50b4a7c3e387fd1
Maurits van der Schee <maurits@vdschee.nl>
2017-03-17 11:36:05 +0100

    add test for soft delete

--- tests/blog_mysql.sql
+++ tests/blog_mysql.sql
@@ -124,7 +124,7 @@ CREATE TABLE `products` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `price` decimal(10,2) NOT NULL,
-  `properties` TEXT NOT NULL,
+  `properties` JSON NOT NULL,
   `created_at` datetime NOT NULL,
   `deleted_at` datetime NULL,
   PRIMARY KEY (`id`)
