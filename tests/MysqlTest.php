b75c5a0bcfa98d1fbb99ababd3ad9d40099e81f5
Karl Hughes <khughes.me@gmail.com>
2017-04-08 17:03:59 -0500

    Making mysql and postgres test suites adaptable

--- tests/MysqlTest.php
+++ tests/MysqlTest.php
@@ -4,6 +4,10 @@ require_once(__DIR__ . '/tests.php');
 
 class MysqlTest extends PHP_CRUD_API_Test
 {
+    const MYSQL_56 = 50600;
+    const MYSQL_57 = 50700;
+    public static $mysql_version;
+
     public static function setUpBeforeClass()
     {
         self::setConfig('MySQL');
@@ -21,8 +25,6 @@ class MysqlTest extends PHP_CRUD_API_Test
             die("Configure database in 'config.php' before running tests.\n");
         }
 
-        $fixture = __DIR__.'/data/blog_'.strtolower(static::$config['dbengine']).'.sql';
-
         $link = mysqli_connect(
             static::$config['hostname'],
             static::$config['username'],
@@ -34,16 +36,138 @@ class MysqlTest extends PHP_CRUD_API_Test
             die("Connect failed: ".mysqli_connect_error()."\n");
         }
 
+        // Note: For some reason this version is formatted:
+        // $mysql_version = main_version * 10000 + minor_version * 100 + sub_version
+        static::$mysql_version = mysqli_get_server_version($link);
+        $seed_file = self::getSeedFile();
+
         mysqli_set_charset($link,'utf8');
 
         $i=0;
-        if (mysqli_multi_query($link, file_get_contents($fixture))) {
+        if (mysqli_multi_query($link, file_get_contents($seed_file))) {
             do { $i++; mysqli_next_result($link); } while (mysqli_more_results($link));
         }
         if (mysqli_errno($link)) {
-            die("Loading '$fixture' failed on statemement #$i with error:\n".mysqli_error($link)."\n");
+            die("Loading '$seed_file' failed on statemement #$i with error:\n".mysqli_error($link)."\n");
         }
 
         mysqli_close($link);
     }
+
+    /**
+     * Gets the path to the seed file based on the version of MySQL
+     *
+     * @return string
+     */
+    protected static function getSeedFile()
+    {
+        if (static::$mysql_version >= self::MYSQL_57) {
+            return __DIR__.'/data/blog_'.strtolower(static::$config['dbengine']).'_57.sql';
+        } elseif (static::$mysql_version >= self::MYSQL_56) {
+            return __DIR__.'/data/blog_'.strtolower(static::$config['dbengine']).'_56.sql';
+        }
+        return __DIR__.'/data/blog_'.strtolower(static::$config['dbengine']).'_55.sql';
+    }
+
+    public function testHidingPasswordColumn()
+    {
+        parent::testHidingPasswordColumn();
+    }
+
+    public function testMissingIntermediateTable()
+    {
+        $test = new API($this, static::$config);
+        $test->get('/users?include=posts,tags');
+        $test->expect('{"users":{"columns":["id","username","location"],"records":[[1,"user1",null]]},"posts":{"relations":{"user_id":"users.id"},"columns":["id","user_id","category_id","content"],"records":[[1,1,1,"blog started"],[2,1,2,"It works!"]]},"post_tags":{"relations":{"post_id":"posts.id"},"columns":["id","post_id","tag_id"],"records":[[1,1,1],[2,1,2],[3,2,1],[4,2,2]]},"tags":{"relations":{"id":"post_tags.tag_id"},"columns":["id","name"],"records":[[1,"funny"],[2,"important"]]}}');
+    }
+
+    public function testEditUserPassword()
+    {
+        parent::testEditUserPassword();
+    }
+
+    public function testEditUserLocation()
+    {
+        parent::testEditUserLocation();
+    }
+
+    public function testListUserLocations()
+    {
+        parent::testListUserLocations();
+    }
+
+    public function testEditUserWithId()
+    {
+        parent::testEditUserWithId();
+    }
+
+    public function testReadOtherUser()
+    {
+        parent::testReadOtherUser();
+    }
+
+    public function testEditOtherUser()
+    {
+        parent::testEditOtherUser();
+    }
+
+    public function testSpatialFilterWithin()
+    {
+        if (static::$mysql_version < self::MYSQL_56) {
+            $this->markTestSkipped("MySQL < 5.6 does not support JSON fields.");
+        }
+        parent::testSpatialFilterWithin();
+    }
+
+
+    public function testListProductsProperties()
+    {
+        if (static::$mysql_version < self::MYSQL_57) {
+            $this->markTestSkipped("MySQL < 5.7 does not support JSON fields.");
+        }
+        parent::testListProductsProperties();
+    }
+
+    public function testReadProductProperties()
+    {
+        if (static::$mysql_version < self::MYSQL_57) {
+            $this->markTestSkipped("MySQL < 5.7 does not support JSON fields.");
+        }
+        parent::testReadProductProperties();
+    }
+
+    public function testWriteProductProperties()
+    {
+        if (static::$mysql_version < self::MYSQL_57) {
+            $this->markTestSkipped("MySQL < 5.7 does not support JSON fields.");
+        }
+        parent::testWriteProductProperties();
+    }
+
+    public function testListProducts()
+    {
+        parent::testListProducts();
+    }
+
+    public function testAddProducts()
+    {
+        if (static::$mysql_version < self::MYSQL_57) {
+            $this->markTestSkipped("MySQL < 5.7 does not support JSON fields.");
+        }
+        parent::testAddProducts();
+    }
+
+    public function testSoftDeleteProducts()
+    {
+        if (static::$mysql_version < self::MYSQL_57) {
+            $test = new API($this, static::$config);
+            $test->delete('/products/1');
+            $test->expect('1');
+            $test->get('/products?columns=id,deleted_at');
+            $test->expect('{"products":{"columns":["id","deleted_at"],"records":[[1,"2013-12-11 11:10:09"]]}}');
+        } else {
+            parent::testSoftDeleteProducts();
+        }
+    }
+
 }
--- tests/PostgresqlTest.php
+++ tests/PostgresqlTest.php
@@ -4,6 +4,9 @@ require_once(__DIR__ . '/tests.php');
 
 class PostgresqlTest extends PHP_CRUD_API_Test
 {
+    public static $gis_installed;
+    public static $pg_server_version;
+
     public static function setUpBeforeClass()
     {
         static::setConfig('PostgreSQL');
@@ -21,8 +24,6 @@ class PostgresqlTest extends PHP_CRUD_API_Test
             die("Configure database in 'config.php' before running tests.\n");
         }
 
-        $fixture = __DIR__.'/data/blog_'.strtolower(static::$config['dbengine']).'.sql';
-
         $e = function ($v) { return str_replace(array('\'','\\'),array('\\\'','\\\\'),$v); };
         $hostname = $e(static::$config['hostname']);
         $database = $e(static::$config['database']);
@@ -36,16 +37,123 @@ class PostgresqlTest extends PHP_CRUD_API_Test
             die("Connect failed: ". pg_last_error());
         }
 
-        $queries = preg_split('/;\s*\n/', file_get_contents($fixture));
+        static::$pg_server_version = pg_version()['server'];
+        $gisInstalled = self::isGisInstalled(
+            pg_fetch_all(
+                pg_query($db, "SELECT * FROM pg_extension;")
+            )
+        );
+        $seed_file = self::getSeedFile();
+        $queries = preg_split('/;\s*\n/', file_get_contents($seed_file));
         array_pop($queries);
 
         foreach ($queries as $i=>$query) {
             if (!pg_query($db, $query.';')) {
                 $i++;
-                die("Loading '$fixture' failed on statemement #$i with error:\n".print_r( pg_last_error($db), true)."\n");
+                die("Loading '$seed_file' failed on statemement #$i with error:\n".print_r( pg_last_error($db), true)."\n");
             }
         }
 
         pg_close($db);
     }
+
+    /**
+     * Gets the path to the seed file based on the version of Postgres and GIS extension
+     *
+     * @return string
+     */
+    protected function getSeedFile()
+    {
+        $filepath = __DIR__.'/data/blog_'.strtolower(static::$config['dbengine']);
+
+        if (version_compare(static::$pg_server_version, '9.4.0') >= 0 ) {
+            $filepath .= '_94';
+        } elseif (version_compare(static::$pg_server_version, '9.2.0') >= 0 ) {
+            $filepath .= '_92';
+        } else {
+            $filepath .= '_91';
+        }
+        if (static::$gis_installed) {
+            $filepath .= '_gis';
+        }
+        return $filepath.'.sql';
+    }
+
+    /**
+     * Determines whether the GIS extension is installed or not based on array of extensions.
+     *
+     * @return boolean
+     */
+    protected function isGisInstalled($extensions = [])
+    {
+        static::$gis_installed = false;
+        if ($extensions) {
+            foreach ($extensions as $extension) {
+                if ($extension['extname'] === 'postgis') {
+                    static::$gis_installed = true;
+                    break;
+                }
+            }
+        }
+        return static::$gis_installed;
+    }
+
+    public function testSpatialFilterWithin()
+    {
+        if (!static::$gis_installed) {
+            $this->markTestSkipped("Postgis not installed");
+        }
+        parent::testSpatialFilterWithin();
+    }
+
+    public function testListProductsProperties()
+    {
+        if (version_compare(static::$pg_server_version, '9.2.0') < 0) {
+            $this->markTestSkipped("Postgres < 9.2.0 does not support JSON fields.");
+        }
+        parent::testListProductsProperties();
+    }
+
+    public function testReadProductProperties()
+    {
+        if (version_compare(static::$pg_server_version, '9.2.0') < 0) {
+            $this->markTestSkipped("Postgres < 9.2.0 does not support JSON fields.");
+        }
+        parent::testReadProductProperties();
+    }
+
+    public function testWriteProductProperties()
+    {
+        if (version_compare(static::$pg_server_version, '9.2.0') < 0) {
+            $this->markTestSkipped("Postgres < 9.2.0 does not support JSON fields.");
+        }
+        parent::testWriteProductProperties();
+    }
+
+    public function testListProducts()
+    {
+        parent::testListProducts();
+    }
+
+    public function testAddProducts()
+    {
+        if (version_compare(static::$pg_server_version, '9.2.0') < 0) {
+            $this->markTestSkipped("Postgres < 9.2.0 does not support JSON fields.");
+        }
+        parent::testAddProducts();
+    }
+
+    public function testSoftDeleteProducts()
+    {
+        if (version_compare(static::$pg_server_version, '9.2.0') < 0) {
+            $test = new API($this, static::$config);
+            $test->delete('/products/1');
+            $test->expect('1');
+            $test->get('/products?columns=id,deleted_at');
+            $test->expect('{"products":{"columns":["id","deleted_at"],"records":[[1,"2013-12-11 11:10:09"]]}}');
+        } else {
+            parent::testSoftDeleteProducts();
+        }
+    }
+
 }
