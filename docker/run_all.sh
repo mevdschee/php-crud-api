61dc1b7d015f21f3ce6f64e198a1ac38da2ca5be
Maurits van der Schee <maurits@vdschee.nl>
2018-09-25 23:40:20 +0200

    Reduce github hits

--- docker/run_all.sh
+++ docker/run_all.sh
@@ -5,7 +5,8 @@ for f in $FILES
 do
 if [[ -d "$f" ]]
 then
+  dir=$(readlink -f ..)
   docker rm "php-crud-api_$f" > /dev/null 2>&1
-  docker run -ti --name "php-crud-api_$f" "php-crud-api:$f"
+  docker run -ti -v $dir:/php-crud-api --name "php-crud-api_$f" "php-crud-api:$f"
 fi
 done
