b5fda866de4d6313b840c0b07b250f8c784b22d8
Maurits van der Schee <maurits@vdschee.nl>
2018-09-30 15:49:02 +0200

    Improve responses

--- src/Tqdev/PhpCrudApi/OpenApi/OpenApiBuilder.php
+++ src/Tqdev/PhpCrudApi/OpenApi/OpenApiBuilder.php
@@ -45,8 +45,19 @@ class OpenApiBuilder
         foreach ($tableNames as $tableName) {
             $this->setPath($tableName);
         }
+        $this->openapi->set("components|responses|pk_integer|description", "inserted primary key value (integer)");
+        $this->openapi->set("components|responses|pk_integer|content|application/json|schema|type", "integer");
+        $this->openapi->set("components|responses|pk_integer|content|application/json|schema|format", "int64");
+        $this->openapi->set("components|responses|pk_string|description", "inserted primary key value (string)");
+        $this->openapi->set("components|responses|pk_string|content|application/json|schema|type", "string");
+        $this->openapi->set("components|responses|pk_string|content|application/json|schema|format", "uuid");
+        $this->openapi->set("components|responses|rows_affected|description", "number of rows affected (integer)");
+        $this->openapi->set("components|responses|rows_affected|content|application/json|schema|type", "integer");
+        $this->openapi->set("components|responses|rows_affected|content|application/json|schema|format", "int64");
         foreach ($tableNames as $tableName) {
             $this->setComponentSchema($tableName);
+            $this->setComponentResponse($tableName);
+            $this->setComponentRequestBody($tableName);
         }
         $this->setComponentParameters();
         foreach ($tableNames as $index => $tableName) {
@@ -61,33 +72,74 @@ class OpenApiBuilder
         $pk = $table->getPk();
         $pkName = $pk ? $pk->getName() : '';
         foreach ($this->operations as $operation => $method) {
+            if (!$pkName && $operation != 'list') {
+                continue;
+            }
             if (in_array($operation, ['list', 'create'])) {
                 $path = sprintf('/records/%s', $tableName);
             } else {
-                if (!$pkName) {
-                    continue;
-                }
                 $path = sprintf('/records/%s/{%s}', $tableName, $pkName);
                 $this->openapi->set("paths|$path|$method|parameters|0|\$ref", "#/components/parameters/pk");
             }
+            if (in_array($operation, ['create', 'update'])) {
+                $this->openapi->set("paths|$path|$method|requestBody|\$ref", "#/components/requestBodies/single_" . urlencode($tableName));
+            }
             $this->openapi->set("paths|$path|$method|tags|0", "$tableName");
             $this->openapi->set("paths|$path|$method|description", "$operation $tableName");
-            $this->openapi->set("paths|$path|$method|responses|200|description", "$operation $tableName succeeded");
+            switch ($operation) {
+                case 'list':
+                    $this->openapi->set("paths|$path|$method|responses|200|\$ref", "#/components/responses/list_of_" . urlencode($tableName));
+                    break;
+                case 'create':
+                    if ($pk->getType() == 'integer') {
+                        $this->openapi->set("paths|$path|$method|responses|200|\$ref", "#/components/responses/pk_integer");
+                    } else {
+                        $this->openapi->set("paths|$path|$method|responses|200|\$ref", "#/components/responses/pk_string");
+                    }
+                    break;
+                case 'read':
+                    $this->openapi->set("paths|$path|$method|responses|200|\$ref", "#/components/responses/single_" . urlencode($tableName));
+                    break;
+                case 'update':
+                case 'delete':
+                case 'increment':
+                    $this->openapi->set("paths|$path|$method|responses|200|\$ref", "#/components/responses/rows_affected");
+                    break;
+            }
+
         }
     }
 
     private function setComponentSchema(String $tableName) /*: void*/
     {
-        $this->openapi->set("components|schemas|$tableName|type", "object");
+        $this->openapi->set("components|schemas|single_$tableName|type", "object");
         $table = $this->reflection->getTable($tableName);
         foreach ($table->columnNames() as $columnName) {
             $column = $table->get($columnName);
             $properties = $this->types[$column->getType()];
             foreach ($properties as $key => $value) {
-                $this->openapi->set("components|schemas|$tableName|properties|$columnName|$key", $value);
+                $this->openapi->set("components|schemas|single_$tableName|properties|$columnName|$key", $value);
             }
-
         }
+        $this->openapi->set("components|schemas|list_of_$tableName|type", "object");
+        $this->openapi->set("components|schemas|list_of_$tableName|properties|count|type", "integer");
+        $this->openapi->set("components|schemas|list_of_$tableName|properties|count|format", "int64");
+        $this->openapi->set("components|schemas|list_of_$tableName|properties|records|type", "array");
+        $this->openapi->set("components|schemas|list_of_$tableName|properties|records|items|\$ref", "#/components/schemas/single_" . urlencode($tableName));
+    }
+
+    private function setComponentResponse(String $tableName) /*: void*/
+    {
+        $this->openapi->set("components|responses|single_$tableName|description", "single $tableName record");
+        $this->openapi->set("components|responses|single_$tableName|content|application/json|schema|\$ref", "#/components/schemas/single_" . urlencode($tableName));
+        $this->openapi->set("components|responses|list_of_$tableName|description", "list of $tableName records");
+        $this->openapi->set("components|responses|list_of_$tableName|content|application/json|schema|\$ref", "#/components/schemas/list_of_" . urlencode($tableName));
+    }
+
+    private function setComponentRequestBody(String $tableName) /*: void*/
+    {
+        $this->openapi->set("components|requestBodies|single_$tableName|description", "single $tableName record");
+        $this->openapi->set("components|requestBodies|single_$tableName|content|application/json|schema|\$ref", "#/components/schemas/single_" . urlencode($tableName));
     }
 
     private function setComponentParameters() /*: void*/
