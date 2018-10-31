6d9d724a723dd5e1a6b120b1f175248038e1d2e0
Maurits van der Schee <maurits@vdschee.nl>
2018-10-31 06:29:28 +0100

    cleanup

--- src/Tqdev/PhpCrudApi/Record/Condition/Condition.php
+++ src/Tqdev/PhpCrudApi/Record/Condition/Condition.php
@@ -50,15 +50,13 @@ abstract class Condition
                 $command = substr($command, 1);
             }
         }
-        if (count($parts) == 3 || (count($parts) == 2 && in_array($command, ['ic', 'is', 'iv']))) {
-            if ($spatial) {
-                if (in_array($command, ['co', 'cr', 'di', 'eq', 'in', 'ov', 'to', 'wi', 'ic', 'is', 'iv'])) {
-                    $condition = new SpatialCondition($field, $command, $parts[2]);
-                }
-            } else {
-                if (in_array($command, ['cs', 'sw', 'ew', 'eq', 'lt', 'le', 'ge', 'gt', 'bt', 'in', 'is'])) {
-                    $condition = new ColumnCondition($field, $command, $parts[2]);
-                }
+        if ($spatial) {
+            if (in_array($command, ['co', 'cr', 'di', 'eq', 'in', 'ov', 'to', 'wi', 'ic', 'is', 'iv'])) {
+                $condition = new SpatialCondition($field, $command, $parts[2]);
+            }
+        } else {
+            if (in_array($command, ['cs', 'sw', 'ew', 'eq', 'lt', 'le', 'ge', 'gt', 'bt', 'in', 'is'])) {
+                $condition = new ColumnCondition($field, $command, $parts[2]);
             }
         }
         if ($negate) {
