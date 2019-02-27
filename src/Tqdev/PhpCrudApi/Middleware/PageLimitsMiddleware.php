b60f3b190fad1aa216a584e01e2031e19cf7b892
Maurits van der Schee <maurits@vdschee.nl>
2019-02-28 00:39:15 +0100

    Implemented #504

--- src/Tqdev/PhpCrudApi/Middleware/PageLimitsMiddleware.php
+++ src/Tqdev/PhpCrudApi/Middleware/PageLimitsMiddleware.php
@@ -2,7 +2,6 @@
 namespace Tqdev\PhpCrudApi\Middleware;
 
 use Tqdev\PhpCrudApi\Column\ReflectionService;
-use Tqdev\PhpCrudApi\Column\Reflection\ReflectedTable;
 use Tqdev\PhpCrudApi\Controller\Responder;
 use Tqdev\PhpCrudApi\Middleware\Base\Middleware;
 use Tqdev\PhpCrudApi\Middleware\Router\Router;
@@ -21,47 +20,27 @@ class PageLimitsMiddleware extends Middleware
         $this->utils = new RequestUtils($reflection);
     }
 
-    private function getMissingOrderParam(ReflectedTable $table): String
-    {
-        $pk = $table->getPk();
-        if (!$pk) {
-            $columnNames = $table->getColumnNames();
-            if (!$columnNames) {
-                return '';
-            }
-            return $columnNames[0];
-        }
-        return $pk->getName();
-    }
-
     public function handle(Request $request): Response
     {
         $operation = $this->utils->getOperation($request);
         if ($operation == 'list') {
-            $tableName = $request->getPathSegment(2);
-            $table = $this->reflection->getTable($tableName);
-            if ($table) {
-                $params = $request->getParams();
-                if (!isset($params['order']) || !$params['order']) {
-                    $params['order'] = array($this->getMissingOrderParam($table));
-                }
-                $maxPage = (int) $this->getProperty('pages', '100');
-                if (isset($params['page']) && $params['page']) {
-                    if (strpos($params['page'][0], ',') === false) {
-                        $params['page'] = array(min($params['page'][0], $maxPage));
-                    } else {
-                        list($page, $size) = explode(',', $params['page'][0], 2);
-                        $params['page'] = array(min($page, $maxPage) . ',' . $size);
-                    }
-                }
-                $maxSize = (int) $this->getProperty('records', '1000');
-                if (!isset($params['size']) || !$params['size']) {
-                    $params['size'] = array($maxSize);
+            $params = $request->getParams();
+            $maxPage = (int) $this->getProperty('pages', '100');
+            if (isset($params['page']) && $params['page']) {
+                if (strpos($params['page'][0], ',') === false) {
+                    $params['page'] = array(min($params['page'][0], $maxPage));
                 } else {
-                    $params['size'] = array(min($params['size'][0], $maxSize));
+                    list($page, $size) = explode(',', $params['page'][0], 2);
+                    $params['page'] = array(min($page, $maxPage) . ',' . $size);
                 }
-                $request->setParams($params);
             }
+            $maxSize = (int) $this->getProperty('records', '1000');
+            if (!isset($params['size']) || !$params['size']) {
+                $params['size'] = array($maxSize);
+            } else {
+                $params['size'] = array(min($params['size'][0], $maxSize));
+            }
+            $request->setParams($params);
         }
         return $this->next->handle($request);
     }
