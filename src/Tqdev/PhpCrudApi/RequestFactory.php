e98d94c4d1fd7295093932dd850a99977dc6bbf6
Maurits van der Schee <maurits@vdschee.nl>
2019-04-20 01:37:38 +0200

    wip

---
+++ src/Tqdev/PhpCrudApi/RequestFactory.php
@@ -0,0 +1,37 @@
+<?php
+namespace Tqdev\PhpCrudApi;
+
+use Nyholm\Psr7Server\ServerRequestCreator;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Psr\Http\Message\ServerRequestInterface;
+
+class RequestFactory
+{
+    public static function fromGlobals(): ServerRequestInterface
+    {
+        $psr17Factory = new Psr17Factory();
+        $creator = new ServerRequestCreator($psr17Factory, $psr17Factory, $psr17Factory, $psr17Factory);
+        return $creator->fromGlobals();
+    }
+
+    public static function fromString(String $request): ServerRequestInterface
+    {
+        $parts = explode("\n\n", trim($request), 2);
+        $lines = explode("\n", $parts[0]);
+        $first = explode(' ', trim(array_shift($lines)), 2);
+        $method = $first[0];
+        $body = isset($parts[1]) ? $parts[1] : '';
+        $url = isset($first[1]) ? $first[1] : '';
+
+        $psr17Factory = new Psr17Factory();
+        $serverRequest = $psr17Factory->createServerRequest($method, $url);
+        if ($body) {
+            $serverRequest = $serverRequest->withBody($body);
+        }
+        foreach ($lines as $line) {
+            list($key, $value) = explode(':', $line, 2);
+            $serverRequest = $serverRequest->withAddedHeader($key, $value);
+        }
+        return $serverRequest;
+    }
+}
---
+++ src/clone.sh
@@ -0,0 +1,19 @@
+git clone git@github.com:php-fig/http-message.git
+mkdir -p Psr/Http/Message
+cp -R http-message/src/* Psr/Http/Message
+rm -Rf http-message
+
+git clone git@github.com:php-fig/http-factory.git
+mkdir -p Psr/Http/Message
+cp -R http-factory/src/* Psr/Http/Message
+rm -Rf http-factory
+
+git clone git@github.com:Nyholm/psr7.git
+mkdir -p Nyholm/Psr7
+cp -R psr7/src/* Nyholm/Psr7
+rm -Rf psr7
+
+git clone git@github.com:Nyholm/psr7-server.git
+mkdir -p Nyholm/Psr7Server
+cp -R psr7-server/src/* Nyholm/Psr7Server
+rm -Rf psr7-server
