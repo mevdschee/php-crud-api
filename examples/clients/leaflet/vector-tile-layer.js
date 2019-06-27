8ed38d28be7c9abbb965d0dc833d296860046246
Maurits van der Schee <maurits@vdschee.nl>
2019-06-27 16:20:42 +0200

    vector tiles wip

--- examples/clients/leaflet/vector-tile-layer.js
+++ examples/clients/leaflet/vector-tile-layer.js
@@ -1,7 +1,7 @@
 /* global L */
 (function() {
 
-    L.GeoJSONURL = L.GeoJSON.extend({
+    L.vectorTileLayer = L.GeoJSON.extend({
 
     includes: L.Evented.prototype,
 
@@ -15,12 +15,45 @@
         L.Util.setOptions(this, extraOptions);
     },
 
+
+/*
+ function long2tile(lon,zoom) { return (Math.floor((lon+180)/360*Math.pow(2,zoom))); }
+ function lat2tile(lat,zoom)  { return (Math.floor((1-Math.log(Math.tan(lat*Math.PI/180) + 1/Math.cos(lat*Math.PI/180))/Math.PI)/2 *Math.pow(2,zoom))); }
+
+Inverse process:
+
+ function tile2long(x,z) {
+  return (x/Math.pow(2,z)*360-180);
+ }
+ function tile2lat(y,z) {
+  var n=Math.PI-2*Math.PI*y/Math.pow(2,z);
+  return (180/Math.PI*Math.atan(0.5*(Math.exp(n)-Math.exp(-n))));
+ }
+
+Example for calculating number of tiles within given extent and zoom-level:
+
+var zoom        = 9;
+var top_tile    = lat2tile(north_edge, zoom); // eg.lat2tile(34.422, 9);
+var left_tile   = lon2tile(west_edge, zoom);
+var bottom_tile = lat2tile(south_edge, zoom);
+var right_tile  = lon2tile(east_edge, zoom);
+var width       = Math.abs(left_tile - right_tile) + 1;
+var height      = Math.abs(top_tile - bottom_tile) + 1;
+
+// total tiles
+var total_tiles = width * height; // -> eg. 377
+*/
+
     _reload: function() {
         if (this.map) {
-            var url = this._expand(this.options.url);
-            this._ajax('GET', url, false, this._update.bind(this));
+            var urls = this._expand(this.options.url);
+            for (var i=0; i<urls.length; i++) {
+                this._ajax('GET', urls[i], false, this._update.bind(this));
+            }
         }
     },
+
+    
 
     _update: function(geoData) {
         this.clearLayers();
@@ -52,7 +85,7 @@
             lon2: northEast.lng,
             bbox: bboxStr
         };
-        return L.Util.template(template, coords);
+        return [L.Util.template(template, coords)];
     },
 
     _ajax: function(method, url, data, callback) {
@@ -74,8 +107,8 @@
 
 });
 
-L.geoJSONURL = function (options) {
-    return new L.GeoJSONURL(options);
+L.vectorTileLayer = function (options) {
+    return new L.vectorTileLayer(options);
 };
 
 }).call(this);
