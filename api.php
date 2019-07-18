a489e7bcf2fccfa70b84551957d0083d594f7c4f
Maurits van der Schee <maurits@vdschee.nl>
2019-07-18 19:41:52 +0200

    better basePath support

--- api.php
+++ api.php
@@ -4810,13 +4810,16 @@ class SimpleRouter implements Router
             return $basePath;
         }
         if (isset($_SERVER['PATH_INFO'])) {
-            $fullPath = array_shift(explode('?',$_SERVER['REQUEST_URI']));
+            $fullPath = explode('?', $_SERVER['REQUEST_URI'])[0];
             $path = $_SERVER['PATH_INFO'];
-            if (substr($fullPath, -1*strlen($path)) == $path) {
-                return substr($fullPath, 0, -1*strlen($path));
+            if (!$path) {
+                return $_SERVER['PHP_SELF'];
+            }
+            if (substr($fullPath, -1 * strlen($path)) == $path) {
+                return substr($fullPath, 0, -1 * strlen($path));
             }
         }
-        return '';
+        return '/';
     }
 
     private function loadPathTree(): PathTree
@@ -4870,15 +4873,10 @@ class SimpleRouter implements Router
 
     private function removeBasePath(ServerRequestInterface $request): ServerRequestInterface
     {
-        if ($this->basePath) {
-            $path = $request->getUri()->getPath();
-            $basePath = rtrim($this->basePath, '/');
-            if (substr($path, 0, strlen($basePath)) == $basePath) {
-                $path = substr($path, strlen($basePath));
-                $request = $request->withUri($request->getUri()->withPath($path));
-            }
-        } elseif (isset($_SERVER['PATH_INFO'])) {
-            $path = $_SERVER['PATH_INFO'];
+        $path = $request->getUri()->getPath();
+        $basePath = rtrim($this->basePath, '/');
+        if (substr($path, 0, strlen($basePath)) == $basePath) {
+            $path = substr($path, strlen($basePath));
             $request = $request->withUri($request->getUri()->withPath($path));
         }
         return $request;
