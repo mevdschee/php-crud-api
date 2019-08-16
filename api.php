c4671ab399a5188fe695c284c9db091441673c43
Maurits van der Schee <maurits@vdschee.nl>
2019-08-16 16:47:22 +0200

    Fix for #568

--- api.php
+++ api.php
@@ -4702,6 +4702,16 @@ namespace Tqdev\PhpCrudApi\Database {
 
         public function convertColumnValue(ReflectedColumn $column): string
         {
+            if ($column->isBoolean()) {
+                switch ($this->driver) {
+                    case 'mysql':
+                        return "IFNULL(IF(?,TRUE,FALSE),NULL)";
+                    case 'pgsql':
+                        return "?";
+                    case 'sqlsrv':
+                        return "?";
+                }
+            }
             if ($column->isBinary()) {
                 switch ($this->driver) {
                     case 'mysql':
@@ -4734,7 +4744,6 @@ namespace Tqdev\PhpCrudApi\Database {
                         return "encode($value::bytea, 'base64') as $value";
                     case 'sqlsrv':
                         return "CASE WHEN $value IS NULL THEN NULL ELSE (SELECT CAST($value as varbinary(max)) FOR XML PATH(''), BINARY BASE64) END as $value";
-
                 }
             }
             if ($column->isGeometry()) {
--- src/Tqdev/PhpCrudApi/Database/ColumnConverter.php
+++ src/Tqdev/PhpCrudApi/Database/ColumnConverter.php
@@ -15,6 +15,16 @@ class ColumnConverter
 
     public function convertColumnValue(ReflectedColumn $column): string
     {
+        if ($column->isBoolean()) {
+            switch ($this->driver) {
+                case 'mysql':
+                    return "IFNULL(IF(?,TRUE,FALSE),NULL)";
+                case 'pgsql':
+                    return "?";
+                case 'sqlsrv':
+                    return "?";
+            }
+        }
         if ($column->isBinary()) {
             switch ($this->driver) {
                 case 'mysql':
@@ -47,7 +57,6 @@ class ColumnConverter
                     return "encode($value::bytea, 'base64') as $value";
                 case 'sqlsrv':
                     return "CASE WHEN $value IS NULL THEN NULL ELSE (SELECT CAST($value as varbinary(max)) FOR XML PATH(''), BINARY BASE64) END as $value";
-
             }
         }
         if ($column->isGeometry()) {
