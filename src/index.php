f162331212d5c2144a5b542609377601ebf88c59
Maurits van der Schee <maurits@vdschee.nl>
2020-10-26 15:13:27 +0100

    update

--- src/index.php
+++ src/index.php
@@ -17,6 +17,12 @@ $config = new Config([
     'password' => 'php-crud-api',
     'database' => 'php-crud-api',
     // 'debug' => false
+'customization.beforeHandler' => function ($operation, $tableName, $request, $environment) {
+  $param = $request->getQueryParams();
+  $param["filter"] = "field1,eq,value1";
+  return $request->withQueryParams($param);            
+}
+
 ]);
 $request = RequestFactory::fromGlobals();
 $api = new Api($config);
