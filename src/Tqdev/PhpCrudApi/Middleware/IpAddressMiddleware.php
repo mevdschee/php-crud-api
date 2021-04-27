8198c228936777a32f2632508bbd91371e829f14
Maurits van der Schee <maurits@vdschee.nl>
2021-04-27 11:27:53 +0200

    use PSR-7 for IP address middleware

--- src/Tqdev/PhpCrudApi/Middleware/IpAddressMiddleware.php
+++ src/Tqdev/PhpCrudApi/Middleware/IpAddressMiddleware.php
@@ -22,7 +22,7 @@ class IpAddressMiddleware extends Middleware
         $this->reflection = $reflection;
     }
 
-    private function callHandler($record, string $operation, ReflectedTable $table) /*: object */
+    private function callHandler(ServerRequestInterface $request, $record, string $operation, ReflectedTable $table) /*: object */
     {
         $context = (array) $record;
         $columnNames = $this->getProperty('columns', '');
@@ -30,7 +30,7 @@ class IpAddressMiddleware extends Middleware
             foreach (explode(',', $columnNames) as $columnName) {
                 if ($table->hasColumn($columnName)) {
                     if ($operation == 'create') {
-                        $context[$columnName] = $_SERVER['REMOTE_ADDR'];
+                        $context[$columnName] = $this->getIpAddress($request);
                     } else {
                         unset($context[$columnName]);
                     }
@@ -38,6 +38,18 @@ class IpAddressMiddleware extends Middleware
             }
         }
         return (object) $context;
+    }
+
+    private function getIpAddress(ServerRequestInterface $request): string
+    {
+        $reverseProxy = $this->getProperty('reverseProxy', '');
+        if ($reverseProxy) {
+            $ipAddress = array_pop($request->getHeader('X-Forwarded-For'));
+        } else {
+            $serverParams = $request->getServerParams();
+            $ipAddress = $serverParams['REMOTE_ADDR'] ?? '127.0.0.1';
+        }
+        return $ipAddress;
     }
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $next): ResponseInterface
@@ -53,10 +65,10 @@ class IpAddressMiddleware extends Middleware
                         $table = $this->reflection->getTable($tableName);
                         if (is_array($record)) {
                             foreach ($record as &$r) {
-                                $r = $this->callHandler($r, $operation, $table);
+                                $r = $this->callHandler($request, $r, $operation, $table);
                             }
                         } else {
-                            $record = $this->callHandler($record, $operation, $table);
+                            $record = $this->callHandler($request, $record, $operation, $table);
                         }
                         $request = $request->withParsedBody($record);
                     }
