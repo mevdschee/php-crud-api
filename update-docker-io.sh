ffc3df84cb8877b5540ac003432129db054f9d85
Maurits van der Schee <maurits@vdschee.nl>
2022-01-21 23:07:44 +0100

    update

--- update-docker-io.sh
+++ update-docker-io.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
-# Stash local changes
-git stash
-
 # Get new tags from remote
 git fetch --tags
 # Get latest tag name
 latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
 # Checkout latest tag
-git checkout $latestTag
+git checkout $latestTag || exit
 # Make docker tag
 dockerTag=$(echo $latestTag | sed s/v/release-/)
 # Build release
 docker build . -t mevdschee/php-crud-api:$dockerTag
 # Build latest
 docker build . -t mevdschee/php-crud-api:latest
-
-# Apply stashed changes
-git stash apply
 
 # Press enter to publish
 echo -n Press enter to publish..; read
