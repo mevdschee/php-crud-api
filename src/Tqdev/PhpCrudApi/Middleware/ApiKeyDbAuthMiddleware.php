27009afbf2667f7973a16ba003b03d64c8628569
Andreas Noteng <andreas@noteng.no>
2022-07-21 12:10:32 +0200

    Do not check api key if request method is OPTIONS

--- src/Tqdev/PhpCrudApi/Middleware/ApiKeyDbAuthMiddleware.php
+++ src/Tqdev/PhpCrudApi/Middleware/ApiKeyDbAuthMiddleware.php
@@ -32,29 +32,32 @@ class ApiKeyDbAuthMiddleware extends Middleware
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $next): ResponseInterface
     {
-        $user = false;
-        $headerName = $this->getProperty('header', 'X-API-Key');
-        $apiKey = RequestUtils::getHeader($request, $headerName);
-        if ($apiKey) {
-            $tableName = $this->getProperty('usersTable', 'users');
-            $table = $this->reflection->getTable($tableName);
-            $apiKeyColumnName = $this->getProperty('apiKeyColumn', 'api_key');
-            $apiKeyColumn = $table->getColumn($apiKeyColumnName);
-            $condition = new ColumnCondition($apiKeyColumn, 'eq', $apiKey);
-            $columnNames = $table->getColumnNames();
-            $columnOrdering = $this->ordering->getDefaultColumnOrdering($table);
-            $users = $this->db->selectAll($table, $columnNames, $condition, $columnOrdering, 0, 1);
-            if (count($users) < 1) {
-                return $this->responder->error(ErrorCode::AUTHENTICATION_FAILED, $apiKey);
+        $method = $request->getMethod();
+        if ($method != 'OPTIONS'){
+            $user = false;
+            $headerName = $this->getProperty('header', 'X-API-Key');
+            $apiKey = RequestUtils::getHeader($request, $headerName);
+            if ($apiKey) {
+                $tableName = $this->getProperty('usersTable', 'users');
+                $table = $this->reflection->getTable($tableName);
+                $apiKeyColumnName = $this->getProperty('apiKeyColumn', 'api_key');
+                $apiKeyColumn = $table->getColumn($apiKeyColumnName);
+                $condition = new ColumnCondition($apiKeyColumn, 'eq', $apiKey);
+                $columnNames = $table->getColumnNames();
+                $columnOrdering = $this->ordering->getDefaultColumnOrdering($table);
+                $users = $this->db->selectAll($table, $columnNames, $condition, $columnOrdering, 0, 1);
+                if (count($users) < 1) {
+                    return $this->responder->error(ErrorCode::AUTHENTICATION_FAILED, $apiKey);
+                }
+                $user = $users[0];
+            } else {
+                $authenticationMode = $this->getProperty('mode', 'required');
+                if ($authenticationMode == 'required') {
+                    return $this->responder->error(ErrorCode::AUTHENTICATION_REQUIRED, '');
+                }
             }
-            $user = $users[0];
-        } else {
-            $authenticationMode = $this->getProperty('mode', 'required');
-            if ($authenticationMode == 'required') {
-                return $this->responder->error(ErrorCode::AUTHENTICATION_REQUIRED, '');
-            }
+            $_SESSION['apiUser'] = $user;
         }
-        $_SESSION['apiUser'] = $user;
         return $next->handle($request);
     }
 }
