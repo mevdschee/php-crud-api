e8daebe15004829e0515c0239470664414e69138
Maurits van der Schee <maurits@vdschee.nl>
2022-10-06 12:01:40 +0200

    Add ConfigInterface

--- api.include.php
+++ api.include.php
@@ -4429,6 +4429,7 @@ namespace Tqdev\PhpCrudApi\Config\Base {
     interface ConfigInterface
     {
         public function getMiddlewares();
+        public function getProperty(string $key, $default = '');
     }
 }
 
